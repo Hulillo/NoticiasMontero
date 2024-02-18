dc2de923d7e62e5c1f946303095183e44d8c9d77
HugoH <hhervellagonzalez@gmail.com>
2024-02-18 21:30:42 +0100

    Ultimos cambios para mergear

--- src/main/java/org/example/noticiasmontero/Inicio.java
+++ src/main/java/org/example/noticiasmontero/Inicio.java
@@ -12,7 +12,7 @@ public class Inicio extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Inicio.class.getResource("inicio.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 650, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Noticias Montero");
         stage.setScene(scene);
         stage.show();
