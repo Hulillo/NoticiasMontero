e4b1b48cc0a2d3cb3fd8b44b55d60ad0b7eba4f2
Julio <juliomonterobenitez@gmail.com>
2024-02-18 17:58:43 +0100

    fix: gestion usuarios y login class

--- src/main/java/org/example/noticiasmontero/Login.java
+++ src/main/java/org/example/noticiasmontero/Login.java
@@ -10,10 +10,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class GestionUsuarios extends Application {
+public class Login extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GestionUsuarios.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Noticias Montero");
         stage.setScene(scene);
